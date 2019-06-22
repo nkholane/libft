af392897443afd1414ba453191ab08111cd849f2
Nokhwezi KHOLANE <nkholane@c4r14s9.wethinkcode.co.za>
2019-06-22 06:34:39 +0200

    Adding strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,19 +14,16 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int i;
-	unsigned int diff;
+	size_t	i;
 
 	i = 0;
-	diff = 0;
 	while (s1[i] == s2[i] && s1[i] != '\0' && (i < n - 1))
 	{
 		i++;
 		if (s1[i] != s2[i])
 		{
-			diff = (unsigned char)s1[i] - (unsigned char)s2[i];
-			return (diff);
+			return (unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
 }

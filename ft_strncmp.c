6df94dcbb934e8735474a1b80d8dc6a5726194b8
Nokhwezi KHOLANE <nkholane@c4r14s9.wethinkcode.co.za>
2019-06-22 06:56:00 +0200

    Yo

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nkholane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 15:09:40 by nkholane          #+#    #+#             */
-/*   Updated: 2019/06/22 05:51:04 by nkholane         ###   ########.fr       */
+/*   Updated: 2019/06/22 06:55:38 by nkholane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,20 @@
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	size_t	size1;
+	size_t	size2;
 
 	i = 0;
+	size1 = ft_strlen(s1);
+	size2 = ft_strlen(s2);
+	if (size1 == '\0' || size2 == '\0')
+		return (size1 - size2);
 	while (s1[i] == s2[i] && s1[i] != '\0' && (i < n - 1))
 	{
 		i++;
 		if (s1[i] != s2[i])
 		{
-			return (unsigned char)s1[i] - (unsigned char)s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
 	}
 	return (0);

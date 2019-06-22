20e711e5b9edc255b23133f6bb325e4c841bc6bf
Nokhwezi KHOLANE <nkholane@c4r14s9.wethinkcode.co.za>
2019-06-22 02:36:37 +0200

    Yo Ok

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: nkholane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 17:54:56 by nkholane          #+#    #+#             */
-/*   Updated: 2019/06/14 12:19:55 by nkholane         ###   ########.fr       */
+/*   Updated: 2019/06/21 23:10:20 by nkholane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,19 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1 && (*s1 == *s2))
+	unsigned int i;
+	unsigned int diff;
+
+	i = 0;
+	diff = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
 	{
-		s1 += 1;
-		s2 += 2;
+		i++;
+		if (s1[i] != s2[i])
+		{
+			diff = (unsigned char)s1[i] - (unsigned char)s2[i];
+			return (diff);
+		}
 	}
-	return (*s1 - *s2);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
-
-/*int		main(void)
-{
-	char str1[15];
-	char str2[15];
-	int ret;
-
-	ft_strcpy(str1, "ababababab");
-	ft_strcpy(str2, "ababababaub");
-
-	ret = ft_strcmp(str1, str2);
-
-	if(ret < 0) 
-	{
-		printf("str1 is less than str2");
-	}
-	else if(ret > 0)
-	{
-		printf("str2 is less than str1");
-	}
-	else
-	{
-		printf("str1 is equal to str2");
-	}
-	return(0);
-}*/
